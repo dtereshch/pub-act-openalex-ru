dc13f6d41c232f4d73c20ee31f682857f04f5e69
Dmitrii Tereshchenko <dtereshch@gmail.com>
2023-08-10 15:59:00 +0300

    Update pub-act-openalex-ru.R
    Final version & comments

--- pub-act-openalex-ru.R
+++ pub-act-openalex-ru.R
@@ -1,12 +1,24 @@
+# R code for downloading OpenAlex data on publications count
+# for case of Russian institutions
+# by D. Tereshchenko
+
+# Loading packages =============================================================
 library(rjson)
 library(purrr)
 library(tidyr)
 library(dplyr)
-#library(data.table)
+library(readr)
 
+# Take a glimpse at the data ===================================================
+
+### First, I load a sample of 25 institutions that are on page 1 of the output. 
+### I know how to filter institutions from 
+### https://docs.openalex.org/api-entities/institutions/filter-institutions 
 url0 <- "https://api.openalex.org/institutions?filter=country_code:ru"
 page0 <- fromJSON(file = url0)
 
+### I am particularly interested in the following variables.
+### Unfortunately, they are on different levels of the lists. 
 page0[["results"]][[1]][["display_name"]]
 page0[["results"]][[1]][["type"]]
 page0[["results"]][[1]][["works_count"]]
@@ -18,40 +30,54 @@ page0[["results"]][[1]][["geo"]][["longitude"]]
 
 page0[["results"]][[1]][["counts_by_year"]][[1]]
 
-as.data.frame(page0[["results"]][[1]][["counts_by_year"]][[1]])
-# map(page0[["results"]], as.data.table)
-# 
-# dt_list <- map(page0[["results"]], as.data.table)
-# dt <- rbindlist(dt_list, fill = TRUE, idcol = T)
+### I practiced converting nested lists into a dataset on this sample.
+sample0 <- as.data.frame(do.call(rbind, page0[["results"]])) 
 
-df0 <- as.data.frame(do.call(rbind, page0[["results"]])) 
-
-df1 <- df0 %>% 
+sample1 <- sample0 %>% 
   select(display_name, type, geo, counts_by_year) %>% 
-  unnest(counts_by_year) %>% 
+  unnest(display_name) %>%
+  unnest(counts_by_year, keep_empty = TRUE) %>% # To overcome the first level of list
   unnest_wider(c(geo, counts_by_year), names_repair = "universal") %>%
   arrange(display_name, year) %>%
   select(display_name, year, 
          works_count, cited_by_count, 
          city, longitude, latitude)
 
+# Downloading the full dataset =================================================
 
-
-
+### From "meta" list we can obtain the total number of institutions
 institutions_count <- page0[["meta"]][["count"]]
-per_page <- 200 # max allowed by OA
-pages_count <- ceiling(institutions_count / per_page)
+
+### Maximum allowed institutions per page is 200 
+per_page <- 200
+
+### Update the URL for json-file page
+url <- paste0(url0, "&per-page=", as.character(per_page))
+
+### Calculate how many pages is needed to download full data set. 
+pages_count <- ceiling(institutions_count / per_page) #=11
 pages <- seq(1, pages_count, 1)
 
-
-url <- paste0(url0, "&per-page=", as.character(per_page))
+### Create a list of json-files
 urls <- map(pages, \(x) paste0(url, "&page=", as.character(x)))
 jsons <- map(urls, \(x) fromJSON(file = x))
-jsons_res <- map(jsons, \(x) x[["results"]])
 
-#bind_rows
-#see above
+### Convert json-files to data.frames
+dfs <- map(jsons, \(x) as.data.frame(do.call(rbind, x[["results"]])))
 
+### Bind data.frames
+df0 <- bind_rows(dfs)
 
+### Unnest columns that still contain lists
+df1 <- df0 %>% 
+  select(display_name, type, geo, counts_by_year) %>% 
+  unnest(display_name) %>%
+  unnest(counts_by_year, keep_empty = TRUE) %>% 
+  unnest_wider(c(geo, counts_by_year), names_repair = "universal") %>%
+  arrange(display_name, year) %>%
+  select(display_name, year, 
+         works_count, cited_by_count, 
+         city, longitude, latitude)
 
-
+# Saving the data set ==========================================================
+df1 %>% write_csv("openalex_ru.csv")
