67acd912716103edef5a182b03f9d26ec8752bd1
Dmitrii Tereshchenko <dtereshch@gmail.com>
2024-05-14 09:44:08 +0300

    Create regional datasets

--- openalex-rus-regions.R
+++ openalex-rus-regions.R
@@ -37,15 +37,10 @@ rus_reg_sf_80[!st_is_valid(rus_reg_sf_80),]
 
 ### Presumably the error is due to the projection transformation 
 ### required to construct adequate maps
+### Probably setting sf_use_s2(FALSE) solves the problem
 st_crs(rus_reg_sf)
 st_crs(rus_reg_sf_80)
 
-### Change crs
-rus_reg_sf <- rus_reg_sf %>% st_set_crs(4326)
-rus_reg_sf_80 <- rus_reg_sf_80 %>% st_set_crs(4326)
-
-mean(st_is_valid(rus_reg_sf))
-mean(st_is_valid(rus_reg_sf_80))
 
 # Joining dataframes ===========================================================
 
@@ -54,23 +49,30 @@ mean(st_is_valid(rus_reg_sf_80))
 ### Convert OpenAlex data to spatial df
 oa_inst_sf <- st_as_sf(x = oa_inst, 
                        coords = c("longitude", "latitude"), 
-                       crs = 4326) %>% st_transform_proj(crs = "+proj=longlat +lon_wrap=180")
+                       crs = 4326) %>% 
+  st_transform_proj(crs = "+proj=longlat +lon_wrap=180")
 
 mean(sf::st_is_valid(oa_inst_sf))
 
 ### Add region column to the OpenAlex data frame
-oa_inst_reg <- st_join(oa_inst_sf, rus_reg_sf["NAME_1"], join = st_intersects, left = TRUE)
+oa_inst_reg <- oa_inst_sf %>% 
+  st_join(rus_reg_sf["NAME_1"], join = st_intersects, left = TRUE) %>%
+  rename(region_shp = NAME_1)
 
-## Regions dataset -------------------------------------------------------------
-#number <- function(x, na.rm = TRUE){return(sum(!is.na(x)))}
+
+## Aggregate data at the regional level ----------------------------------------
 
 oa_reg <- oa_inst_reg %>% 
   st_drop_geometry() %>%
-  rename(region_shp = NAME_1) %>%
   group_by(region_shp, year, type) %>%
   summarise(works_count = sum(works_count, na.rm = TRUE),
             cited_by_count = sum(cited_by_count, na.rm = TRUE),
             inst_count = n()) %>%
   ungroup()
 
-oa_edu_reg <- oa_reg %>% filter(type == "education") %>% select(-type)
+## Save data ===================================================================
+
+oa_inst_reg %>% write_csv(paste0("openalex_w_reg_", Sys.Date() ,".csv"))
+oa_reg %>% write_csv(paste0("openalex_regions_", Sys.Date() ,".csv"))
+
+rm(list = ls())
